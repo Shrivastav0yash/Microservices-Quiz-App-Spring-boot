1f70fee145a5c18a773770945e2452ef0b9a62e9
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-04-16 02:31:14 +0530

    Rename app

--- src/main/java/com/microservices/quiz_app/QuizappApplication.java
+++ src/main/java/com/microservices/quiz_app/QuizappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter9MircoservicesApplication {
+public class QuizappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter9MircoservicesApplication.class, args);
+		SpringApplication.run(QuizappApplication.class, args);
 	}
 
 }
