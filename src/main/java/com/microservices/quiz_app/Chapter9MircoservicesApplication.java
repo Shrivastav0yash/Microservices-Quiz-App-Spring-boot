67ca8739ae77932cc4b69d911f191c08d4dbacaf
Shrivastav0yash <yashshrivastav8938@gmail.com>
2026-04-16 01:22:49 +0530

    Init Project

---
+++ src/main/java/com/microservices/quiz_app/Chapter9MircoservicesApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.quiz_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter9MircoservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter9MircoservicesApplication.class, args);
+	}
+
+}
